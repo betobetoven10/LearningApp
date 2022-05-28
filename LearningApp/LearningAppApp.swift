c44ec784955e7e1b8013986e0bededa283449c69
Alberto Madueno <alberto_mn@outlook.com>
2022-05-28 14:41:34 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Alberto Madueño Navarrete on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
